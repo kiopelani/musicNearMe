17638be37ba69c89168f433b253b2d38fe518ee4
Kiope Gyzen <kiopelani@gmail.com>
2015-02-14 16:18:37 -0800

    search via bandsintown api for concerts by artist

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,15 @@
 // Declare app level module which depends on views, and components
 angular.module('musicNearMe', [])
 
-.controller('ConcertsCtrl', ['$scope', function($scope){
-  $scope.hey = "hey";
-}]);
+.controller('ConcertsCtrl', function($scope, $http){
+  $scope.concerts;
+  // $scope.artist = "Taylor%20Swift";
+  // function that parses artist into appropriate format
+  $scope.findConcerts = function(){
+    if(!$scope.artist || $scope.artist === ""){return;}
+
+    $http.jsonp('http://api.bandsintown.com/artists/'+$scope.artist+'/events.json?api_version=2.0&app_id=music_near_me&callback=JSON_CALLBACK').then(function(response){
+      console.log(response.data);
+    });
+  };
+});
