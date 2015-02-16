cc21419ec84a15dd580a596b0da2ce050a472602
Kiope Gyzen <kiopelani@gmail.com>
2015-02-15 17:35:32 -0800

    added basic styling

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,24 @@
 
 var controllers = angular.module('musicNearMe.controllers', []);
 
-controllers.controller('MainCtrl', function($scope){});
+controllers.controller('MainCtrl', function($scope, $location){
+
+});
 
 controllers.controller('ConcertsCtrl', function($scope, $http, $location, markers){
   $scope.error;
   $scope.concerts = [];
   $scope.concertView = "";
+  $scope.showMap = false;
+
+  $scope.reload = function(){
+    $scope.concertView = '';
+    $scope.concerts = [];
+    $scope.error = "";
+    $scope.showMap = false;
+    $location.path('/');
+
+  };
 
   $scope.findConcerts = function(){
     if(!$scope.artist || $scope.artist === ""){return;}
@@ -14,24 +26,28 @@ controllers.controller('ConcertsCtrl', function($scope, $http, $location, marker
     $scope.concerts = [];
     $scope.error = "";
     $scope.currArtist = "";
+    $scope.concertView = "";
 
     $http.jsonp('http://api.bandsintown.com/artists/'+$scope.artist+'/events.json?api_version=2.0&app_id=music_near_me&callback=JSON_CALLBACK').then(function(response){
       console.log(response.data);
       var data = response.data;
       if(data.length < 1){
         $scope.error = $scope.artist + " has no upcoming concerts at this time.";
-        markers.makeMap();
-        $scope.artistImage = "http://cdn.flaticon.com/png/256/42901.png";
+        $scope.showMap = false;
+        $scope.artistImage = "";
+        $scope.concertView = '';
+        $location.path('/');
       }
       else {
         $scope.currArtist = data[0].artists[0].name;
         $scope.artistImage = data[0].artists[0].image_url;
         markers.makeMap(data[0].venue.latitude, data[0].venue.longitude);
+        $scope.showMap = true;
         $scope.addConcerts(data);
+        $scope.concertView = 'main';
+        $location.path('main');
       }
-      $scope.artist = "";
-      $scope.concertView = 'main';
-      $location.path('main');
+        $scope.artist = "";
 
     });
   };
